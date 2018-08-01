89c5f6e584d78fced8ee04fc6c581b8b219d6d03
xiaolei.guo <xiaolei.guo@sinobbd.com>
2018-08-01 20:58:25 +0800

    updat

--- bookdetails-service/handlers/handlers_test.go
+++ bookdetails-service/handlers/handlers_test.go
@@ -5,12 +5,44 @@ import (
 	pb "bookinfo/pb/details"
 	"bookinfo/pb/comments"
 	"context"
-	"reflect"
+	"github.com/stretchr/testify/mock"
+	"github.com/davecgh/go-spew/spew"
 )
+
+type MockBooksDetailsServer struct {
+	mock.Mock
+}
+
+func (this MockBooksDetailsServer) Detail(ctx context.Context,req *pb.DetailReq) (*pb.DetailResp, error) {
+	args := this.Mock.Called(ctx, req)
+	spew.Dump(args)
+	return args.Get(0).(*pb.DetailResp), args.Error(1)
+}
 
 var booksDetailsServer = NewService()
 
 func TestBooksDetailsV1Detail(t *testing.T) {
+
+	mockBooksDetailsServer := MockBooksDetailsServer{}
+	mockBooksDetailsServer.On(
+		"Detail",
+		nil,
+		&pb.DetailReq{Id: 1},
+	).Return(pb.DetailResp{
+		Code: 200,
+		Msg:  "success",
+		Data: &pb.DetailRespData{
+			Id:    1,
+			Name:  "西游记",
+			Intro: "《西游记》是中国古代第一部浪漫主义章回体长篇神魔小说。现存明刊百回本《西游记》均无作者署名。清代学者吴玉搢等首先提出《西游记》作者是明代吴承恩 [1]  。这部小说以“唐僧取经”这一历史事件为蓝本，通过作者的艺术加工，深刻地描绘了当时的社会现实。全书主要描写了孙悟空出世及大闹天宫后，遇见了唐僧、猪八戒和沙僧三人，西行取经，一路降妖伏魔，经历了九九八十一难，终于到达西天见到如来佛祖，最终五圣成真的故事。",
+			Comments: []*comments.Comment{
+				{Content: "测试评论1", CreatedAt: "2018-06-20 06:59:40"},
+				{Content: "测试评论2", CreatedAt: "2018-06-20 06:59:40"},
+				{Content: "测试评论3", CreatedAt: "2018-06-20 06:59:40"},
+			},
+		},
+	}, nil)
+
 	testData := struct {
 		in   int64
 		want pb.DetailResp
@@ -32,8 +64,10 @@ func TestBooksDetailsV1Detail(t *testing.T) {
 		},
 	}
 
+
 	req := &pb.DetailReq{Id: testData.in}
-	resp, err := booksDetailsServer.Detail(context.Background(), req)
+	resp, err := mockBooksDetailsServer.Detail(context.Background(), req)
+
 
 	if err != nil {
 		t.Error("books-details /v1/detail", err)
@@ -43,7 +77,7 @@ func TestBooksDetailsV1Detail(t *testing.T) {
 		resp.Msg != testData.want.Msg ||
 		resp.Data.Id != testData.want.Data.Id ||
 		resp.Data.Name != testData.want.Data.Name ||
-		resp.Data.Intro != testData.want.Data.Intro{
+		resp.Data.Intro != testData.want.Data.Intro {
 		t.Error(
 			"books-details /v1/detail,",
 			"req [", req, " ],",
